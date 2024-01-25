47c950a3fe88970c7b673600f07aeac7cc2f7d1f
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2024-01-25 17:03:02 +0100

    FIRST PUSH

---
+++ js/navbar.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const icon1 = document.querySelector(".icon1");
+const icon2 = document.querySelector(".icon2");
+const icon3 = document.querySelector(".icon3");
+
+function displayMenu(){
+    if (nav.classList.contains("hidden")) {
+      
+        nav.classList.remove("hidden");
+        icon1.classList.add("rotated1");
+        icon2.classList.add("rotated2");
+        icon3.classList.add("rotated3");
+        console.log("works");
+    }
+
+    else {
+        nav.classList.add("hidden");
+        icon1.classList.remove("rotated1");
+        icon2.classList.remove("rotated2");
+        icon3.classList.remove("rotated3");
+        console.log("works");
+    }
+}
+
+console.log(icon1);
+menu.addEventListener('click', displayMenu)
