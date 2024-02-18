c2e98a2a2ae1d1fd6c60df1e780a99a2a36611cd
martinsk91 <132287423+martinsk91@users.noreply.github.com>
2024-02-18 09:15:10 +0100

    test

--- js/rendercarouselle.js
+++ js/rendercarouselle.js
@@ -6,7 +6,8 @@ console.log(carouselle);
 
 function renderPosts(){
     for(let i = 0; i < post.length; i++){
-        carouselle.innerHTML +=`<a href="specific.html?id=${post[i].id}/"><div><span><img src="${post[i].acf.image}"><h2>${post[i].acf.title}</h2></span></div></a>`
+        carouselle.innerHTML= "<p>hello</p>"
+        // carouselle.innerHTML +=`<a href="specific.html?id=${post[i].id}/"><div><span><img src="${post[i].acf.image}"><h2>${post[i].acf.title}</h2></span></div></a>`
     }
    
 }
